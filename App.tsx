f614dda576aea4f486d3f6d01881c32e65006f48
itseduardolima <eduardolima2417@gmail.com>
2023-12-07 13:46:49 -0400

    chore: first config

---
+++ App.tsx
@@ -0,0 +1,18 @@
+import Principal from "./src/Principal";
+import { Themes } from "./src/styles/themes";
+
+import {NativeBaseProvider, StatusBar} from "native-base"
+
+export default function App() {
+  return (
+
+    <NativeBaseProvider theme={Themes} >
+
+      <StatusBar backgroundColor={Themes.colors.blue[800]} />
+
+      <Principal />
+
+    </NativeBaseProvider>
+
+  )
+}
